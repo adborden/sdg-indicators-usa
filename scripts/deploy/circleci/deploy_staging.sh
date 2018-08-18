c4ea96ef629b092dd7a42414979e29c936f84159
Brock Fanning <brockfanning@gmail.com>
2018-08-17 21:27:27 -0400

    Config and deployment changes.

--- scripts/deploy/circleci/deploy_staging.sh
+++ scripts/deploy/circleci/deploy_staging.sh
@@ -6,7 +6,7 @@ git config --global user.name $GH_NAME
 # Git to use this key, we need to hack the SSH key:
 sed -i -e 's/Host gh-stg/Host gh-stg\n  HostName github.com/g' ~/.ssh/config
 # Now we can use "gh-stg" below to identify the SSH key to use.
-git clone git@gh-stg:$GH_ORG_STG/sdg-indicators-1.git out
+git clone git@gh-stg:$GH_ORG_STG/sdg-indicators-usa.git out
 
 cd out
 git checkout gh-pages || git checkout --orphan gh-pages
